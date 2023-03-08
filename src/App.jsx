c5b47b349de6587e10ac92bc69494f77139411ed
Iqbal-Baloch <iqbalsyedmuhammad1@gmail.com>
2023-03-08 23:40:42 +0500

    add info page dummy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import {Info} from "./Pages/Info" ;
 function App() {
 
   return (
     <div className="App">
+      <Info/ >
     </div>
   )
 }
---
+++ src/Pages/Info.jsx
@@ -0,0 +1,10 @@
+
+export const Info = () => {
+  return (
+    <div>
+        <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default Info ;

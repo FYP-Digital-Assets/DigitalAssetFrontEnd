2b31579a7a6e194d69c29e9b9a91d0c575ca5b2d
Iqbal-Baloch <iqbalsyedmuhammad1@gmail.com>
2023-03-10 01:29:48 +0500

    implemented pagenotfound page

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,11 +1,12 @@
-
+import E404 from "../assets/E404.jsx" ;
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../App.css"
 export const Info = () => {
     return (
-      <div>
-          <h1>Error 404 page not found</h1>
+      <div className="d-flex justify-content-center mt-5">
+          <E404/>
       </div>
     )
   }
-  
   export default Info ;
   

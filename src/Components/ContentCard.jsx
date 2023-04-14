a3ff7e06225fe63051747263054153f0db8b7800
Iqbal-Baloch <iqbalsyedmuhammad1@gmail.com>
2023-04-15 00:53:48 +0500

    edited content Card

--- src/Components/ContentCard.jsx
+++ src/Components/ContentCard.jsx
@@ -1,4 +1,4 @@
-import {Card, Col} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
 import "./ContentCard.css"
 export function ContentCard(prop){
   //styles objects
@@ -11,21 +11,25 @@ export function ContentCard(prop){
       
       <Card  className='content_card_style border'>
         {prop.type?<div className='card_typeDiv'>{prop.type}</div>:<></>}
-        <div className='p-1'>
-          <Card.Img variant="top" className='card_ImgStyle' src={prop.img} />
-          <Card.Body className='p-1'>
+        <div >
+          <div className='p-1'>
+            <Card.Img variant="top" className='card_ImgStyle' src={prop.img} />
+          </div>
+          
+          <Card.Body className='p-1 mb-2'>
             <Card.Title className='content_card_title'>{prop.title}</Card.Title>
             <img src={prop.authorImg}  className='card_authorImgStyle'/>
             <b>{prop.author}</b><br/>
-            <small style={priceTagStyle}>Price: {prop.price}</small>
+            
           </Card.Body>
+          <Card.Footer className='d-flex justify-content-between flex-column'>
+            <small className='d-flex justify-content-between'><span>Purchase:</span>{prop.price}</small>
+            <small className='d-flex justify-content-between'><span>View:</span> {prop.price}</small>
+            <small className='d-flex justify-content-between'><span>License</span> {prop.price}</small>
+          </Card.Footer>
         </div>
       </Card>
       
     );
   }
-  export function ContentCard2(props){
-    <div>
-
-    </div>
-  }
+  

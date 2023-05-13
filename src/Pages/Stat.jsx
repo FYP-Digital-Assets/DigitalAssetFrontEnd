079640986da051782b6d5f71179b6fd0c46ea6b7
shankar-01 <shankar.bscsf19@iba-suk.edu.pk>
2023-05-14 00:25:24 +0500

    Stat page updated

--- src/Pages/Stat.jsx
+++ src/Pages/Stat.jsx
@@ -3,22 +3,24 @@ import { useState } from "react"
 export default function Stat(props){
     //call data from backend using useEffect
     const[trending, setTrending] = useState([
-        {title:"abc", thumbnail:"1683743582292", prices:[10, 20, 30], view:10, 
+        {title:"abc", thumbnail:"1683743582292.jpeg", prices:[10, 20, 30], view:10, 
         licesors:["a", "b"]},
-        {title:"cde", thumbnail:"1683743582292", prices:[12, 20, 30], view:100, 
+        {title:"cde", thumbnail:"1683743582292.jpeg", prices:[12, 20, 30], view:100, 
         licesors:["a", "b"]},
-        {title:"fgh", thumbnail:"1683743582292", prices:[18, 20, 30], view:200, 
+        {title:"fgh", thumbnail:"1683743582292.jpeg", prices:[18, 20, 30], view:200, 
         licesors:["a", "b"]}
     ])
     return <>
-        <table>
+        <table style={{width:"100%"}}>
             <tbody>
                 {trending?trending.map(
                     (obj, i)=>{
                         return (
-                            <tr>
+                            <tr key={i}>
                                 <td>{i}</td>
-                                <td><img src={`http://localhost:4000/thumbnails${obj.thumbnail}`}/></td>
+                                <td><img src={`http://localhost:4000/thumbnail/${obj.thumbnail}`}
+                                style={{width:"8rem", height:"8rem", objectFit:"cover"}}
+                                /></td>
                                 <td>{obj.title}</td>
                                 <td>{obj.prices[0]}</td>
                                 <td>{obj.prices[1]}</td>

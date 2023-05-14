f664361ec00b2c90e3bb88f3ad598cc94d3b16b3
JaveriaIrfan686 <76628927+JaveriaIrfan686@users.noreply.github.com>
2023-05-14 16:04:10 -0400

    Cards for about us

---
+++ src/Components/Card.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './AboutUs.css';
+
+
+const Card = ({ user }) => {
+  const [showPopUp, setShowPopUp] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowPopUp(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowPopUp(false);
+  };
+
+  return (
+    <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <img src={user.picture} alt={user.name} />
+      <div className="card-content">
+      
+        <h2>{user.name}</h2>
+        
+        <button>More</button>
+      </div>
+      {showPopUp && (
+        <div className="pop-up1">
+          <p>{user.bio}</p>  
+          <p>{user.email}</p>
+        </div>
+        
+      )}
+    
+    </div>
+  );
+};
+
+export default Card;
+
+

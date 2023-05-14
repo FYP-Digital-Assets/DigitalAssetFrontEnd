f3305bb9a8bf4f0e3375f7d01c9ff6ba68b91ab8
JaveriaIrfan686 <76628927+JaveriaIrfan686@users.noreply.github.com>
2023-05-14 15:58:29 -0400

    Main file for about us

---
+++ src/Pages/App.js
@@ -0,0 +1,15 @@
+
+import AboutUs from './AboutUs';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <AboutUs></AboutUs>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;

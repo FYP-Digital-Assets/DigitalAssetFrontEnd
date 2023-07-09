0d12fbb2f3372ca511d560cca6888d7b549ff633
shankar-01 <shankar.bscsf19@iba-suk.edu.pk>
2023-07-09 16:00:28 +0500

    hyperlink added to History page content

--- src/Pages/History.jsx
+++ src/Pages/History.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import { useState } from "react"
-
+import { Link } from "react-router-dom";
 export default function History(props){
     const [history, setHistory] = useState([]);
     const actions = ["Viewed", "Licensed", "Owned"]
@@ -19,9 +19,11 @@ export default function History(props){
     return (<>
         {history?history.map((obj,i)=>{
             console.log(obj)
-            return <div style={{display:"flex", justifyContent:"space-between"
-            ,borderRadius:"0.5em", background:"lightgray", border:"2px solid black",
-            fontSize:"1.5rem",alignItems:"center", margin:"0.7em"
+            return <Link to={`/auth/${obj.address}`} className="removeLinkEffect">
+            <div style={{display:"flex", justifyContent:"space-between"
+            ,borderRadius:"0.5em", background:"white", border:"1px solid black",
+            fontSize:"1.5rem",alignItems:"center", margin:"0.7em", boxShadow:"0.5em 0.5em 0.5em 0.5em lightgray",
+            padding:"0.5em"
             }} key={i}>
                 <b>{i+1}</b>
                 <img src={`http://localhost:4000/thumbnail/${obj.thumbnail}`} style={{width:"5rem", height:"4rem", objectFit:"cover"}} key={i}/>
@@ -29,6 +31,7 @@ export default function History(props){
                 <p style={{color:actionColor[obj.action]}}>{actions[obj.action]}</p>
                 <p>{new Date(obj.date).toISOString().substring(0, 10)}</p>
             </div>
+            </Link>
         }):<h1>
             No History
         </h1>
